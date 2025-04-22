434cad81d4d2d26fe22286cd2ec3fe4e3cc32bec
OralbekUrusbekov <oralbekurusbekov2022@gmail.com>
2025-04-22 21:59:33 +0500

    Add level completion logic with player destination check and Level Complete screen

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,54 +3,62 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener{
-	
+
 	public Image image;
 	public Graphics graphics;
 	public Thread gameThread;
-	
+
 	public static final int GAME_WIDTH = 1080;
 	public static final int GAME_HEIGHT = 720;
-	int level;
+	int level, framesAtFinish;
 	Wall walls[] = new Wall[5];
-	
+	Instruction levelComplete;
+
 	Player fireboy, watergirl;
 
+	public boolean gameFinished;
+
 	public GamePanel(int level) {
-		this.setFocusable(true); 
-		this.addKeyListener(this); 
+		this.setFocusable(true); // make everything in this class appear on the screen
+		this.addKeyListener(this); // start listening for keyboard input
 
 		this.setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
-		
+		levelComplete = new Instruction(500, 300, "Level Complete");
+
 		this.level = level;
-		
+		gameFinished = false;
+		framesAtFinish = 0;
+
+		//TEMPORARY
 		walls[0] = new Wall(0, 0, 1080, 30);
 		walls[1] = new Wall(0, 0, 30, 720);
 		walls[2] = new Wall(0, 690, 1080, 720);
 		walls[3] = new Wall(1050, 0, 1080, 720);
 		walls[4] = new Wall(30, 600, 1000, 630);
-		fireboy = new Player('w', 'a', 'd', 100, 100, Color.red);
-		watergirl = new Player((char)38, (char)37, (char)39, 200, 100, Color.blue);
-	
-		
+		fireboy = new Player('w', 'a', 'd', 100, 100, Color.red, 400, 540);
+		watergirl = new Player((char)38, (char)37, (char)39, 200, 100, Color.blue, 500, 540);
+		//TEMPORARY
+
 		gameThread = new Thread(this);
 		gameThread.start();
 	}
-	
+
 	public void draw(Graphics g) {
-		for(int i=0;i<5;i++) walls[i].draw(g);
 		fireboy.draw(g);
 		watergirl.draw(g);
+		for(int i=0;i<5;i++) walls[i].draw(g);
+		if(gameFinished) levelComplete.draw(g);
 	}
-	
+
 	public void paint(Graphics g) {
-		image = createImage(GAME_WIDTH, GAME_HEIGHT); 
+		image = createImage(GAME_WIDTH, GAME_HEIGHT); // draw off screen
 		graphics = image.getGraphics();
-		draw(graphics); 
-		g.drawImage(image, 0, 0, this); 
+		draw(graphics); // update the positions of everything on the screen
+		g.drawImage(image, 0, 0, this); // redraw everything on the screen
 	}
-	
+
 	public void playerCollidesWall() {
-		
+
 		for(int i=0;i<walls.length;i++) {
 			if(fireboy.x + Player.PlayerWidth > walls[i].x1 && fireboy.x < walls[i].x2) {
 				if(fireboy.y + Player.PlayerHeight >= walls[i].y1 && fireboy.y + Player.PlayerHeight <= walls[i].y1 + Player.PlayerHeight/2) {
@@ -63,17 +71,17 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 					fireboy.y = walls[i].y2 + 1;
 				}
 			}
-			
+
 			if(fireboy.y < walls[i].y2 && fireboy.y + Player.PlayerHeight > walls[i].y1) {
 				if(fireboy.x + Player.PlayerWidth >= walls[i].x1 && fireboy.x + Player.PlayerWidth <= walls[i].x1 + Player.PlayerWidth/2) {
 					fireboy.x = walls[i].x1 - Player.PlayerWidth;
-				}				
+				}
 				else if(fireboy.x <= walls[i].x2 && fireboy.x >= walls[i].x2 - Player.PlayerWidth/2) {
 					fireboy.x = walls[i].x2;
 				}
-			}		
+			}
 		}
-		
+
 		for(int i=0;i<walls.length;i++) {
 			if(watergirl.x + Player.PlayerWidth > walls[i].x1 && watergirl.x < walls[i].x2) {
 				if(watergirl.y + Player.PlayerHeight >= walls[i].y1 && watergirl.y + Player.PlayerHeight <= walls[i].y1 + Player.PlayerHeight/2) {
@@ -86,26 +94,38 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 					watergirl.y = walls[i].y2 + 1;
 				}
 			}
-			
+
 			if(watergirl.y < walls[i].y2 && watergirl.y + Player.PlayerHeight > walls[i].y1) {
 				if(watergirl.x + Player.PlayerWidth >= walls[i].x1 && watergirl.x + Player.PlayerWidth <= walls[i].x1 + Player.PlayerWidth/2) {
 					watergirl.x = walls[i].x1 - Player.PlayerWidth;
-				}				
+				}
 				else if(watergirl.x <= walls[i].x2 && watergirl.x >= walls[i].x2 - Player.PlayerWidth/2) {
 					watergirl.x = walls[i].x2;
 				}
-			}		
+			}
 		}
 	}
-	
+
+	public void playersAtDoors() {
+		if(fireboy.atDestination() && watergirl.atDestination()) {
+			framesAtFinish++;
+
+			if(framesAtFinish >= 60) {
+				gameFinished = true;
+			}
+		}
+		else framesAtFinish = 0;
+	}
+
 	public void checkCollision() {
 		playerCollidesWall();
+		playersAtDoors();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 
-		
 	}
 
 	@Override
@@ -119,7 +139,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 		fireboy.keyReleased(e);
 		watergirl.keyReleased(e);
 	}
-	
+
 	public void move() {
 		fireboy.move();
 		watergirl.move();
@@ -133,18 +153,21 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 		double delta = 0;
 		long now;
 
-		while (true) { 
+		while (true) { // this is the infinite game loop
 			now = System.nanoTime();
 			delta = delta + (now - lastTime) / ns;
 			lastTime = now;
 
 			if (delta >= 1) {
-				move();
-				checkCollision();
+				if(!gameFinished) {
+					move();
+					checkCollision();
+				}
 				repaint();
 				delta--;
 			}
 		}
-		
+
 	}
 }
+
