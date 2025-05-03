3f9e7c61cde4de458ca50deab0bcb1d3cf6dd829
OralbekUrusbekov <oralbekurusbekov2022@gmail.com>
2025-05-03 08:34:41 +0500

    Add image-based rendering for Pool class and improve documentation changed Pool

--- src/Pool.java
+++ src/Pool.java
@@ -1,19 +1,27 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.ImageIcon;
+import java.awt.Image;
 
 public class Pool extends Rectangle{
 
     public static final int POOL_DEPTH = 10;
     public int length, type;
     public int startX, startY;
-    Color colour;
+    String filePath;
+    Image pool;
 
     public Pool(int startX, int startY, int length, int type) {
         super(startX, startY, length, POOL_DEPTH);
         this.type = type;
-        if(type == 0) colour = Color.green;
-        else if(type == 1) colour = Color.red;
-        else colour = Color.blue;
+
+        if(type == 0) filePath = "../Images/Acid.png";
+        else if(type == 1) filePath = "../Images/Lava.png";
+        else filePath = "../Images/Water.png";
+
+        pool = new ImageIcon(filePath).getImage();
+        pool = pool.getScaledInstance(length, 10, Image.SCALE_DEFAULT);
+
 
         this.startX = startX;
         this.startY = startY;
@@ -21,7 +29,6 @@ public class Pool extends Rectangle{
     }
 
     public void draw(Graphics g) {
-        g.setColor(colour);
-        g.fillRect(x, y, length, POOL_DEPTH);
+        g.drawImage(pool, x, y, null);
     }
 }
